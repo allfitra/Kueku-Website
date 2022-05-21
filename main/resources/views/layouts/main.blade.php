31ece85e26f7e24097940e8a770c4815edd6e71a
Fozan Bebe Moreno <fozbebemoreno@gmail.com>
2022-05-21 20:22:22 +0700

    Menambah front end untuk cart system

--- main/resources/views/layouts/main.blade.php
+++ main/resources/views/layouts/main.blade.php
@@ -9,15 +9,15 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        
+
     <title>Kueku | Toko Kue Online</title>
 
-    {{-- font-google poppins --}}
+    {{-- Font-Google Poppins --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 
-    {{-- style css --}}
+    {{-- Style CSS --}}
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/login-signup.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
@@ -36,8 +36,8 @@
     @include('partials.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- main/resources/views/partials/navbar.blade.php
+++ main/resources/views/partials/navbar.blade.php
@@ -20,28 +20,35 @@
             </ul>
 
             @auth
-            <ul class="navbar-nav dropdown ml-auto fw-bold">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi bi-person-fill"></i> {{ auth()->user()->name }}
-                  </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="/#"><i class="bi bi-person-fill"></i> My Account</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <form action="/logout" method="post">
-                    @csrf
-                    <li><button class="dropdown-item" type="submit"><i class="bi bi-box-arrow-in-left"></i> Logout</button></li>
-                    </form>
+                <ul class="navbar-nav dropdown ml-auto fw-bold">
+                    <li class="nav-item px-2 {{ $title === 'cart' ? 'active' : '' }}">
+                        <a class="nav-link" href="/cart"><i class="bi bi-cart-check"></i> Cart</a>
+                    </li>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <i class="bi bi-person-fill"></i> {{ auth()->user()->name }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/#"><i class="bi bi-person-fill"></i> My Account</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <li><button class="dropdown-item" type="submit"><i class="bi bi-box-arrow-in-left"></i>
+                                    Logout</button></li>
+                        </form>
+                    </ul>
                 </ul>
-            </ul>
             @else
-            <ul class="navbar-nav ml-auto fw-bold">
-                <li class="nav-item px-2 {{ $title === 'login' ? 'active' : '' }}">
-                    <a class="nav-link" href="/login">LOGIN</a>
-                </li>
-                <li class="nav-item px-2 {{ $title === 'register' ? 'active' : '' }}">
-                    <a class="nav-link" href="/register">SIGN UP</a>
-                </li>
-            </ul>
+                <ul class="navbar-nav ml-auto fw-bold">
+                    <li class="nav-item px-2 {{ $title === 'login' ? 'active' : '' }}">
+                        <a class="nav-link" href="/login">LOGIN</a>
+                    </li>
+                    <li class="nav-item px-2 {{ $title === 'register' ? 'active' : '' }}">
+                        <a class="nav-link" href="/register">SIGN UP</a>
+                    </li>
+                </ul>
             @endauth
 
         </div>
